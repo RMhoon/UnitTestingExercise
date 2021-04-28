411187a843fdc9d8a6fdf6abd846a079a1c6556f
RMhoon <r_mhoon@hotmail.com>
2021-04-27 21:03:59 -0400

    Added unit test and code

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,77 +1,91 @@
 using System;
 using Xunit;
-
+using UnitTestingExercise;
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(2,3,5,10)]
+        [InlineData(2,2,5,9)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var obj = new UnitTestMethods();
 
             //Act
-
+            var actual = obj.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,8,2)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var obj = new UnitTestMethods();
 
             //Act
-
+            var actual = obj.Subtract(minuend, subtrhend);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5,5,25)]
+        [InlineData(10,3,30)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var obj = new UnitTestMethods();
 
             //Act
-
+            var actual = obj.Multiply(num1, num2);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10,2,5)]
+        [InlineData(20,2,10)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var obj = new UnitTestMethods();
 
             //Act
-
+            var actual = obj.Divide(num1, num2);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Fact]
-        public void YourMethodName()
+        public void GetSalutation()
         {
             //Arrange
-
+            var obj = new UnitTestMethods();
+            string expected = "Hello World";
             //Act
-
+            var actual =obj.GetSalutation();
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void NoteRate()
         {
             //Arrange
-
+            var obj = new UnitTestMethods();
+            decimal expected = (decimal) 4.5;
             //Act
-
+            var actual = obj.NoteRate();
             //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -9,18 +9,41 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
 
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+        // Keep track of which number is getting passed as minuend and subtrahend
 
         // Create a Multiply method that passes 2 integers
+        public int Multiply(int n, int x)
+        {
+            return n * x;
+        }
 
         // Create a Divide method that passes 2 integers
-
+        public int Divide(int n , int x)
+        {
+            return n / x;
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public string GetSalutation()
+        {
+            return "Hello World";
+        }
 
-        
+        public decimal NoteRate()
+        {
+            return (decimal) 4.50;
+        }
+
     }
 }
